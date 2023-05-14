37e805f6c26c8f3fa2bb7f9ae5e1c45d22701c16
sharon <sharondoroth@live.com>
2023-05-14 13:57:25 +0300

    week6day2exercises

--- week6/day2/exercises/exercise2/script.js
+++ week6/day2/exercises/exercise2/script.js
@@ -1,3 +1,58 @@
+//exercise2
 function winBattle() {
   return true;
 }
+
+//result
+const winBattle = () => true;
+
+const experiencePoints = winBattle() ? 10 : 1;
+console.log(experiencePoints);
+
+//exercise3
+const isString = (value) => (typeof value === "string" ? true : false);
+console.log(isString("hello")); // true
+console.log(isString([1, 2, 4, 0])); // false
+
+//exercise4
+const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+
+colors.forEach((color, index) => {
+  console.log(`${index + 1}# choice is ${color}.`);
+});
+
+if (colors.includes("Violet")) {
+  console.log("Yeah");
+} else {
+  console.log("No...");
+}
+
+//exercise5
+const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+const ordinal = ["th", "st", "nd", "rd"];
+
+colors.forEach((color, index) => {
+  const ordinalIndex = index < 3 ? index + 1 : 0;
+  const ordinalIndicator = ordinal[ordinalIndex] || ordinal[0];
+  console.log(`${index + 1}${ordinalIndicator} choice is ${color}.`);
+});
+
+if (colors.includes("Violet")) {
+  console.log("Yeah");
+} else {
+  console.log("No...");
+}
+
+//exercise6
+let bankAmount = 5000;
+const vatPercentage = 17;
+const expenses = ["+200", "-100", "+146", "+167", "-2900"];
+
+for (let i = 0; i < expenses.length; i++) {
+  const expense = parseFloat(expenses[i]);
+  const taxAmount = (vatPercentage / 100) * Math.abs(expense);
+  expenses[i] = (expense >= 0 ? "+" : "-") + (expense + taxAmount);
+  bankAmount += expense + taxAmount;
+}
+
+console.log("Bank Account standing:", bankAmount);
